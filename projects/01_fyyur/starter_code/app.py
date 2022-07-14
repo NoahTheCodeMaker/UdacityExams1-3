7304560ccb5b1bc1aaa604766efa21a173da2cbd
Noah Dragoon <noahdragoon@hotmail.com>
2022-07-14 16:43:25 -0400

    Added flask migrate integration with local database

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -1,12 +1,11 @@
-#----------------------------------------------------------------------------#
 # Imports
-#----------------------------------------------------------------------------#
 
 import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
@@ -15,20 +14,21 @@ from forms import *
 import collections
 import collections.abc
 collections.Callable = collections.abc.Callable
-#----------------------------------------------------------------------------#
+
 # App Config.
-#----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
 
-#----------------------------------------------------------------------------#
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Models.
-#----------------------------------------------------------------------------#
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
