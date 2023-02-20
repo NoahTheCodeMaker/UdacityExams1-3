dd2099854467ab58bcd636bb0d2b54ead7795ad5
Noah <noahdragoon@hotmail.com>
2023-02-19 19:53:31 -0500

    Added new error handler for test cases.

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -265,6 +265,14 @@ def create_app(test_config=None):
       "error": 404,
       "message": "resource not found"
     }), 404
+  
+  @app.errorhandler(405)
+  def not_allowed(error):
+    return jsonify({
+      "success": False,
+      "error": 405,
+      "message": "method not allowed"
+    }), 405
 
   @app.errorhandler(422)
   def unprocessable_entity(error):
