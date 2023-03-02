ce2ca077b07f6efe171542ce2b3a3b70148493e9
Noah <noahdragoon@hotmail.com>
2023-03-02 14:25:22 -0500

    Completed the api.py with all endpoints

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -1,11 +1,9 @@
-import os
 import traceback
 from flask import Flask, request, jsonify, abort
-from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
+from .database.models import db, db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
@@ -13,12 +11,13 @@ setup_db(app)
 CORS(app)
 
 '''
-!! Uncomment the line below these comments to initialize the datbase
+!! The line below these comments will initialize
+!! the database, running this function will add one.
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this function will add one
 '''
-# db_drop_and_create_all()
+
+db_drop_and_create_all()
 
 ## ROUTES
 
@@ -55,46 +54,72 @@ def drinks_detail_viewer(payload):
         traceback.print_exc()
         abort(500)
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+# Drink creation endpoint for Managers
 @app.route('/drinks', methods=['POST'])
 @requires_auth(permission='post:drinks')
 def create_drinks(payload):
-    request.get_json().get("", None)
+    if 'title' and 'recipe' not in request.get_json():
+        abort(422)
+    try:
+        title = request.get_json().get('title', None)
+        recipe = request.get_json().get('recipe', None)
+        new_drink = Drink(
+            title=title,
+            recipe=json.dumps(recipe)
+        )
+        db.session.add(new_drink)
+        db.session.commit()
+        return jsonify({
+            "success": True,
+            "drink": [new_drink.long()]
+        })
+    except:
+        db.session.rollback()
+        traceback.print_exc()
+        abort(422)
+    finally:
+      db.session.close()
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth(permission='patch:drinks')
+def update_drinks(payload, id):
+    try:
+        title = request.get_json().get('title', None)
+        recipe = request.get_json().get('recipe', None)
+        drink = Drink.query.filter_by(id=id).one()
+        drink.title = title
+        drink.recipe = json.dumps(recipe)
+        db.session.commit()
+        return jsonify({
+            "success": True,
+            'drinks': [drink.long()]
+        })
+    except:
+        traceback.print_exc()
+        db.session.rollback()
+        abort(404)
+    finally:
+      db.session.close()
 
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth(permission='delete:drinks')
+def delete_drinks(payload, id):
+    try:
+        drink = Drink.query.filter_by(id=id).one()
+        drink.delete()
+        db.session.commit()
+    except:
+        traceback.print_exc()
+        db.session.rollback()
+        abort(404)
+    finally:
+      db.session.close()
+    return jsonify ({
+        "success": True,
+        "delete": id
+    })
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
-
-## Error Handling
+## Standard Error Handlers
 
 @app.errorhandler(400)
 def bad_request(error):
